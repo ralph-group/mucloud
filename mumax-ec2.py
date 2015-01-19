2ecbde01484fdc83169adde203ae670fd36a04d0
Colin Jermain <cjermain@gmail.com>
2015-01-19 17:51:22 -0500

    Update config and script, including ssh tunneling

--- mumax-ec2.py
+++ mumax-ec2.py
@@ -9,6 +9,7 @@ __version__ = 1.0
 __TAG__ = u'mumax-ec2'
 __STATUS__ = u'status'
 __READY__ = 1
+PORT = 35367
 
 # TODO: Compare mumax-ec2 tag with version
 
@@ -17,18 +18,21 @@ import paramiko
 import sys, os
 from time import sleep
 import select
+from sshtunnel import SSHTunnelForwarder
+
 
 # Start the config parser
 import ConfigParser
 config = ConfigParser.ConfigParser()
-config.read("./config.ini")
+config.read(os.path.dirname(os.path.realpath(__file__))+"/config.ini")
 
 # Connect to Amazon Web Services (AWS)
 conn = boto.ec2.connect_to_region(config.get('EC2', 'Region'),
             aws_access_key_id=config.get('EC2', 'AccessID'),
             aws_secret_access_key=config.get('EC2', 'SecretKey'))
             
-search_condition = lambda i: (i.state == u'running' and 
+search_condition = lambda i: (
+    i.state == u'running' and 
     __TAG__ in i.tags and 
     __STATUS__ in i.tags and
     i.tags[__STATUS__] == __READY__
@@ -38,7 +42,7 @@ instances          = conn.get_only_instances()
 ready_instances     = [i for i in instances if search_condition(i)]
 
 
-def launch_instance(conn):
+def launch_instance():
     """ Launch a new AWS instance """
     print "Creating a new instance of %s" % config.get('EC2', 'Image')
     reservation = conn.run_instances(
@@ -53,38 +57,10 @@ def launch_instance(conn):
     return instance
 
 
-def stop_instance(conn, instance):
-    """ Stops an AWS instance """
-    print "Stopping instance"
-    conn.stop_instances(instance_ids=[instance.id])
-
-
-def terminate_instance(conn, instance):
-    """ Stops an AWS instance """
-    print "Terminating instance"
-    conn.terminate_instances(instance_ids=[instance.id])
-
-
-def put_input_file(ssh, filename):
-    """ Put the file in the input directory """
-    print "Transferring input file to instance"
-    sftp = ssh.open_sftp()
-    sftp.put(filename, "input/"+filename)
-
-
-def get_output_files(ssh, filename):
-    """ Get the simulation files and put them in the 
-    current directory
+def get_ready_instance():
+    """ Returns an instance from the ready list or launches 
+    a new instance upon prompt
     """
-    print "Receiving output files from instance"
-    sftp = ssh.open_sftp()
-    sftp.get() #TODO: Implement the getting of the files
-
-
-def run(job_name, script_file):
-    """Run the mumax input file on a ready instance
-    """
-
     if len(ready_instances) == 0:
         print "There are no instances waiting to be used."
         answer = raw_input("Create a new instance for this job? [Yn]: ")
@@ -94,61 +70,109 @@ def run(job_name, script_file):
             while instance.state != u'running':
                 sleep(0.5)
             print "Instance is ready"
-
-            instace.remove_tag(__STATUS__)
-            try:
-                # Establish connection
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(instance.public_dns_name, 
-                    username='ec2-user', 
-                    key_filename=config.get('EC2', 'PrivateKeyFile')
-                )
- 
-                put_input_file(ssh, script_file)
-
-                transport = ssh.get_transport()
-                channel = transport.open_session()
-
-                channel.exec_command("mumax3 input/%s" % script_file)
-
-                # TODO: Test blocking ability
-                while True:
-                    try:
-                        rl, wl, xl = select.select([channel], [], [], 0.0)
-                        if len(rl) > 0:
-                            print channel.recv(1024)
-                    except KeyboardInterrupt:
-                        print "Caught Ctrl-C"
-                        channel.close()
-
-                        try:
-                            # Try to kill the process
-                            ssh.get_transport().open_session().exec_command(
-                                "kill -9 `ps -fu | grep mumax3 | grep -v grep | awk '{print $2}'`"
-                            )
-                        except:
-                            pass
-
-                        ssh.close()
-
-                print "Finished simulations"
-
-                get_output_files(ssh)
-
-                answer = raw_input("Terminate the instance? [Yn]: ")
-                if len(answer) == 0 or answer.startswith(("Y", "y")):
-                    terminate_instance(instance)
-                else:
-                    answer = raw_input("Stop the instance? [Yn]: ")
-                    if len(answer) == 0 or answer.startswith(("Y", "y")):
-                        stop_instance(instance)
-                    else:
-                        print "The instance has been left running"
         else:
             print "No instance will be launched"
+            return None
+    else:
+        instance = ready_instances[0] # Select the 1st ready instance
 
-        print "Mumax-ec2 has finished"
+
+def stop_instance(instance):
+    """ Stops an AWS instance """
+    print "Stopping instance"
+    conn.stop_instances(instance_ids=[instance.id])
+
+
+def terminate_instance(instance):
+    """ Stops an AWS instance """
+    print "Terminating instance"
+    conn.terminate_instances(instance_ids=[instance.id])
+
+
+
+def run(job_name, local_input_file):
+    """Run the mumax input file on a ready instance
+    """
+
+    instance = get_ready_instance()
+    if instance == None: return None
+
+    instace.remove_tag(__STATUS__)
+    try:
+        # Establish connection
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(instance.public_dns_name, 
+            username=config.get('EC2', 'User'), 
+            key_filename=config.get('EC2', 'PrivateKeyFile')
+        )
+        sftp = ssh.open_sftp()
+
+        basename = os.path.basename(local_input_file)
+        directory = "/home/%s" % config.get('EC2', 'User')
+        input_file = "%s/input/%s" % (directory, basename)
+        output_dir = "%s/output/%s" % (directory, basename)
+
+        print "Transferring input file to instance"
+        sftp.put(local_input_file, input_file)
+
+        print "Start port forwarding"
+        server = SSHTunnelForwarder(
+            ssh_address=(instance.public_dns_name, PORT),
+            ssh_user=config.get('EC2', 'User'),
+            ssh_private_key=config.get('EC2', 'PrivateKeyFile'),
+            remote_bind_address=('127.0.0.1', PORT)
+        )
+        server.start()
+
+        transport = ssh.get_transport()
+        channel = transport.open_session()
+
+
+        binary = "%s/mumax3/%s" % (directory, config.get('EC2', 'MuMaxBinary'))
+        channel.exec_command("%s -http=:%i -o=%s %s" % (binary, PORT, output_dir, input_file))
+
+        #TODO: Test blocking ability
+        while True:
+            try:
+                rl, wl, xl = select.select([channel], [], [], 0.0)
+                if len(rl) > 0:
+                    print channel.recv(1024)
+            except KeyboardInterrupt:
+                print "Caught Ctrl-C"
+                channel.close()
+
+                try:
+                    # Try to kill the process
+                    ssh.get_transport().open_session().exec_command(
+                        "kill -9 `ps -fu | grep mumax3 | grep -v grep | awk '{print $2}'`"
+                    )
+                except:
+                    pass
+
+                ssh.close()
+
+        print "Receiving output files from instance"
+        sftp.chdir()
+        files = sftp.listdir()
+        for item in files:
+            sftp.get(files, local_data) #TODO: Actually implement file transfer
+
+        print "Stopping port forwarding"
+        server.stop()
+
+        answer = raw_input("Terminate the instance? [Yn]: ")
+        if len(answer) == 0 or answer.startswith(("Y", "y")):
+            terminate_instance(instance)
+        else:
+            instance.add_tag(__STATUS__, __READY__)
+            answer = raw_input("Stop the instance? [Yn]: ")
+            if len(answer) == 0 or answer.startswith(("Y", "y")):
+                stop_instance(instance)
+            else:
+                print "The instance has been left running"
+
+    print "Mumax-ec2 has finished"
 
 
 if __name__ == '__main__':
